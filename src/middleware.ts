7a7912c83414a2a56c1b220dc96229253e816887
Felipe Garcia <felipecpg@gmail.com>
2025-10-23 10:03:17 -0400

    first commit

---
+++ src/middleware.ts
@@ -0,0 +1,23 @@
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth({
+  pages: {
+    signIn: "/login",
+  },
+});
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api/auth (authentication endpoints)
+     * - login (login page)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - public folder
+     */
+    "/((?!api/auth|login|_next/static|_next/image|favicon.ico|logo.svg).*)",
+  ],
+};
+
